e8b8a923392546138eff6a9c5dc008f81e45d10a
ildario <ildariario@gmail.com>
2022-03-22 13:44:01 +0300

    Task №10 was added to paragraphs 20.8 and 21.1

--- 20/20.8.py
+++ 20/20.8.py
@@ -161,6 +161,42 @@
 #     print(f'{i_place+1} место: {result_list[i_place][0]} ({result_list[i_place][1]})')
 # 10 ==========================================================================
 
+# # Этап 1 ======================================
+
+# syms_str = 'abc'
+# nums_tpl = (10, 20, 30, 40)
+#
+# pairs = [(syms_str[i_elem], nums_tpl[i_elem])
+#          for i_elem in range(len(syms_str))]
+# print(pairs)
+
+# # Этап 2 ======================================
+#
+# def shortest_seq_range(string, tpl):
+#     return min(len(string), len(tpl))
+#
+#
+# syms_str = 'abcd'
+# nums_tpl = (10, 20, 30, 40)
+#
+# pairs = [(syms_str[i_elem], nums_tpl[i_elem])
+#          for i_elem in range(shortest_seq_range(syms_str, nums_tpl))]
+# print(pairs)
+
+# Этап 3 ======================================
+
+def shortest_seq_range(string, tpl):
+    return min(len(string), len(tpl))
+
+
+syms_str = 'abcd'
+nums_tpl = (10, 20, 30, 40)
+
+pairs = ((syms_str[i_elem], nums_tpl[i_elem])
+         for i_elem in range(shortest_seq_range(syms_str, nums_tpl)))
+print(pairs)
+for i_elem in pairs:
+    print(i_elem)
 # 11 ==========================================================================
 
 # 12 ==========================================================================
